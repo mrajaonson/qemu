b3ee9e5f0d54b57b5ffacaaee2278765ed510845
mrajaonson <m.rajaonson@posteo.net>
2022-12-14 23:11:03 +0100

    update start ubuntu

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # Qemu
+
+## QEMU installation
+
+* https://www.qemu.org/
+
+```shell
+# install
+brew install qemu
+
+# version
+qemu-system-x86_64 --version
+```
+
+## Start VM
+
+```shell
+# create VM
+./start.sh --install
+
+# start VM
+./start.sh
+```
--- start.sh
+++ start.sh
@@ -1,11 +1,43 @@
-qemu-system-x86_64 \
-  -accel hvf \
-  -smp 2 \
-  -m 2G \
-  -device usb-tablet \
-  -usb \
-  -nographic \
-  -device virtio-net,netdev=vmnic \
-  -netdev user,id=vmnic,hostfwd=tcp:127.0.0.1:9001-:22 \
-  -drive file=debian11.qcow2,if=virtio \
-  -cdrom debian-11.5.0-amd64-netinst.iso
+#!/bin/zsh
+
+main() {
+  source .env
+  CDROM=${CDROM}
+  DRIVE_FILE=${DRIVE_FILE}
+
+  case "$1" in
+    "--install")
+      install
+      ;;
+    "--debian")
+      startDebian
+      ;;
+    "--ubuntu")
+      startUbuntu
+      ;;
+    *)
+      startUbuntu
+      ;;
+  esac
+}
+
+install() {
+  qemu-img create -f qcow2 ./"$DRIVE_FILE" 20G
+  sudo chmod +x start.sh
+}
+
+startUbuntu() {
+  qemu-system-x86_64 \
+    -m 4G \
+    -vga virtio \
+    -usb \
+    -device usb-tablet \
+    -machine type=q35,accel=hvf \
+    -smp 2 \
+    -cdrom "$CDROM" \
+    -drive file="$DRIVE_FILE",if=virtio \
+    -cpu Nehalem
+}
+
+main "$@"
+exit
